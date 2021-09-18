cd0dad28ff8c088e9cb5349d72d1020de4017316
Natalya Pletneva <nntalita@gmail.com>
2021-09-18 18:32:20 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,11 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-    service.calculate(amount, registered);
+    long actual = service.calculate(amount, registered);
+    long expected = 30;
+    boolean passed = expected == actual;
+
+    System.out.println(passed);
   }
 }
 
